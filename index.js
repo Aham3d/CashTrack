6a3dd6a67d5117984c5f560a16784ed8f09cebb3
Moin Ahamed <ahamedjust@gmail.com>
2023-05-21 18:20:35 +0530

    selecting the els

---
+++ index.js
@@ -0,0 +1,29 @@
+// elements
+const balanceEl = document.querySelector('.balance .value'); 
+const incomeTotalEl = document.querySelector('.income-total'); 
+const outcomeTotalEl = document.querySelector('.outcome-total'); 
+const incomeEl = document.querySelector('#income'); 
+const expenseEl = document.querySelector('#expense'); 
+const allEl = document.querySelector('#all'); 
+const incomeList = document.querySelector('#income .list');
+const expenseList = document.querySelector('#outcome .list'); 
+const allList = document.querySelector('#all .list'); 
+
+// btns
+const expenseBtn = document.querySelector('.tab1'); 
+const incomeBtn = document.querySelector('.tab2'); 
+const allBtn = document.querySelector('tab3');
+
+// inputs 
+const addExpense = document.querySelector('.add-expense'); 
+const expenseTitle = document.querySelector('.expense-title=input'); 
+const expenseAmout = document.querySelector('.expense-amout-input');
+
+const addincome = document.querySelector('.add-income'); 
+const incomeTitle = document.querySelector('.income-title=input'); 
+const incomeAmout = document.querySelector('.income-amout-input');
+
+
+
+
+
